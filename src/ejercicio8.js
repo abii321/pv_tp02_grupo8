b5225a8e78b681ec3e75c1c7128590d43c5f020d
Abigail <abigail.teran321@gmail.com>
2025-04-13 22:04:51 -0300

    Reparando TP2 Abigail Teran

--- src/ejercicio8.js
+++
@@ -1,5 +0,0 @@
-//.Declarar una función con nombre calcularMayor() y pasarle como 
-// parámetros (numero1,numero2). Dentro de la función mostrar un alert que diga cuál de los 
-// números ingresados es mayor. En caso de ser iguales, tendrá que indicarlo a través de un 
-// alert también. Invocar la función y enviar los argumentos con diferentes números para probar.
- 
