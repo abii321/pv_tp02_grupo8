f629a31d9d710e413fe9949c9d22fc3ef0faccbf
altarifa <abrillucerotarifa@gmail.com>
2025-04-14 15:29:57 -0300

    Ejercicios 1, 6, 11, 16 Abril Tarifa

--- src/ejercicio6.js
+++ src/ejercicio6.js
@@ -1,5 +1,6 @@
 const edades = [18, 22, 20, 25, 30, 27, 24, 19];
 
+console.log("Edades:", edades);
 const calcularPromedio = (arr) => {
     let suma = 0;
     for (let i = 0; i < arr.length; i++) {
