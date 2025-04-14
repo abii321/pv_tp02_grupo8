b4721ba74e7b7c9555f9e3142fb25727627a7a85
lujancansino <celestelujancansino@gmail.com>
2025-04-14 00:33:46 -0300

    modified:   src/ejercicio14.js 	modified:   src/ejercicio4.js 	modified:   src/ejercicio9.js

--- src/ejercicio14.js
+++
@@ -1,2 +0,0 @@
-// Escribir una función llamada duplicar que reciba un arreglo de números y retorne un
-// nuevo arreglo donde cada número esté multiplicado por dos (2).
--- src/ejercicio9.js
+++
@@ -1,2 +0,0 @@
-// Solicitar al usuario que ingrese su nombre, luego muestre un mensaje de bienvenida:
-// Hola (aquí el nombre del usuario), bienvenido a Programación Visual!
