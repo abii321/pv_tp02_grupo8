e0be43ab3bdd95ac1cb9aa5abe13ef54bd198483
soffiro <sofiarodriguez07v@gmail.com>
2025-04-20 11:28:53 -0300

    Modificacion punto 15  por Sofia Rodriguez

--- src/ejercicio15.js
+++ src/ejercicio15.js
@@ -1,16 +1,13 @@
-function retornarMes(numero) {
+const retornarMes = (numero) => {
     const meses = [
         "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
         "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
     ];
 
-    if (numero >= 1 && numero <= 12) {
-        return meses[numero - 1];
-    } else {
-        return "No corresponde a un mes del año.";
-    }
-}
-let numeroMes = parseInt(prompt("Ingrese un número del 1 al 12:"));
-let resultado = retornarMes(numeroMes);
+    return (numero >= 1 && numero <= 12) ? meses[numero - 1] : "No corresponde a un mes del año.";
+};
+
+const numeroMes = parseInt(prompt("Ingrese un número del 1 al 12:"));
+const resultado = retornarMes(numeroMes);
 
 console.log("Resultado: " + resultado);
