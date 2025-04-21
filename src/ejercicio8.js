d46f81232256bc8348dd28bc06ddcab45290719a
orellanaariana <cococonpitula@gmail.com>
2025-04-20 23:12:23 -0300

    ejercicio  18 y 23 Ariana Orellana

--- src/ejercicio8.js
+++ src/ejercicio8.js
@@ -2,7 +2,7 @@
 // parámetros (numero1,numero2). Dentro de la función mostrar un alert que diga cuál de los 
 // números ingresados es mayor. En caso de ser iguales, tendrá que indicarlo a través de un 
 // alert también. Invocar la función y enviar los argumentos con diferentes números para probar.
-function calcularMayor(numero1, numero2) {
+let calcularMayor=(numero1, numero2)=> {
     if (numero1 > numero2) {
     alert(numero1 + " es mayor que " + numero2);
     } else if (numero2 > numero1) {
